4533241ad237a42df15e28bd40ac1b5dffcaba08
Alex <adrdra88@gmail.com>
2021-11-17 19:49:18 +0100

    switch case

--- js/script.js
+++ js/script.js
@@ -1,19 +1,83 @@
 
-
-
-// for(i = 0; i < 64; i++){
-    //  let box  = document.createElement('div');
-    //  box.classList.add('box');
-    //  console.log(box)
-    //  container.append(box);
-    // }
     
-const container = document.querySelector('.container');
-let items = '';
+    const container = document.querySelector('.container');
+    const button = document.getElementById('btn');
+    let selectValue = '';
 
-for(i = 1; i <= 100; i++){
-    items += `<div class="box">${i}</div>`;
+
+    button.addEventListener('click', () =>{
+    selectValue = document.getElementById('level').value;
+    console.log(selectValue)
     
-}
-container.innerHTML = items;
+    switch(selectValue){
+        case 'easy':
+            
+            const addBox = () => {
+              
+               const node = document.createElement('div');  
+               node.className = 'box';
+                
+                return node
+            }
+            
+            for(let i = 0; i < 100; i++){
+                const item = addBox();
+                item.innerText = i + 1;
+                container.appendChild(item);
+        
+                item.addEventListener('click', function(){
+                    console.log(this);
+                    this.classList.add('active');
+                })
+            }
+        break;
+        
+        case 'medium':
+            const addBox2 = () => {
+              
+                const node = document.createElement('div');  
+                node.className = 'box2';
+                 
+                 return node
+             }
+             
+             for(let i = 0; i < 81; i++){
+                 const item = addBox2();
+                 item.innerText = i + 1;
+                 container.appendChild(item);
+         
+                 item.addEventListener('click', function(){
+                     console.log(this);
+                     this.classList.add('active');
+                 })
+             }
+        break;     
+
+        case 'hard':
+            const addBox3 = () => {
+              
+                const node = document.createElement('div');  
+                node.className = 'box3';
+                 
+                 return node
+             }
+             
+             for(let i = 0; i < 49; i++){
+                 const item = addBox3();
+                 item.innerText = i + 1;
+                 container.appendChild(item);
+         
+                 item.addEventListener('click', function(){
+                     console.log(this);
+                     this.classList.add('active');
+                 })
+             }
+    }        
+
+   
+})
+
+
+
+
 
