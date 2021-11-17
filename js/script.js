404cb135360d8cd7f6803da7f8313e86b485f5f2
Alex <adrdra88@gmail.com>
2021-11-17 14:39:34 +0100

    layout

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 
 
+
+// for(i = 0; i < 64; i++){
+    //  let box  = document.createElement('div');
+    //  box.classList.add('box');
+    //  console.log(box)
+    //  container.append(box);
+    // }
+    
 const container = document.querySelector('.container');
 let items = '';
 
-// for(i = 0; i < 64; i++){
-//  let box  = document.createElement('div');
-//  box.classList.add('box');
-//  console.log(box)
-//  container.append(box);
-// }
-
-
-for(i = 0; i < 64; i++){
-    items += `<div class="box"></div>`;
+for(i = 1; i <= 100; i++){
+    items += `<div class="box">${i}</div>`;
     
 }
 container.innerHTML = items;
-   
+
